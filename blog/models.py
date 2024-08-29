c5a5ead147a72cc0ddb394a83a539db54795be53
Kendrick <kendricks715@gmail.com>
2024-08-29 12:53:03 -0500

    Models & Migrate
    modelo de las tablas de datos, migraciones neesarias y ajustes al gitignore

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Entrada(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha_publicacion = models.DateTimeField()
+
+    def __str__(self):
+        return self.titulo
